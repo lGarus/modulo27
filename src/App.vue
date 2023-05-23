f10fd946e0157335d7f2e840ba3d98908448778e
lGarus <128258828+lGarus@users.noreply.github.com>
2023-05-23 16:10:20 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+let a = Number
+let b =  Number
+
+const calcula = function(a, b) {
+  return a + b
+}
+
+</script>
+
+<template>
+
+  <input type="{{ a }}">
+  <input type="{{ b }}">
+  <h1>{{ calcula }}</h1>
+
+</template>
+
+<style scoped>
+
+</style>
